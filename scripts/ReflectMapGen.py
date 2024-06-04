f45791d3c2cb4d6eac33420a561595481897a4e3
derkder <1026305391@qq.com>
2024-06-04 20:40:34 +0800

    feat: reflection map generator init

---
+++ scripts/ReflectMapGen.py
@@ -0,0 +1,22 @@
+from falcor import *
+
+def render_graph_ReflectMapGen():
+    g = RenderGraph("ReflectionMapGen")
+    AccumulatePass = createPass("AccumulatePass", {'enabled': True, 'precisionMode': 'Single'})
+    g.addPass(AccumulatePass, "AccumulatePass")
+    ToneMapper = createPass("ToneMapper", {'autoExposure': False, 'exposureCompensation': 0.0})
+    g.addPass(ToneMapper, "ToneMapper")
+    ReflectMapGen = createPass("ReflectMapGen", {'maxBounces': 3})
+    g.addPass(ReflectMapGen, "ReflectMapGen")
+    VBufferRT = createPass("VBufferRT", {'samplePattern': 'Stratified', 'sampleCount': 16})
+    g.addPass(VBufferRT, "VBufferRT")
+    g.addEdge("AccumulatePass.output", "ToneMapper.src")
+    g.addEdge("VBufferRT.vbuffer", "ReflectMapGen.vbuffer")
+    g.addEdge("VBufferRT.viewW", "ReflectMapGen.viewW")
+    g.addEdge("ReflectMapGen.color", "AccumulatePass.input")
+    g.markOutput("ToneMapper.dst")
+    return g
+
+ReflectMapGen = render_graph_ReflectMapGen()
+try: m.addGraph(ReflectMapGen)
+except NameError: None
